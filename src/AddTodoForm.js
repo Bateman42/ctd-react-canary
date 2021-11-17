24e11d4141537b75c77f53d59bc2af27d25acf09
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-11-16 19:15:34 -0500

    Control add todo input compled

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 function AddTodoForm(props){
 
+    const [todoTitle, setTodoTitle] = React.useState('');
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    };
+
     const handleAddTodo = (event) => {
+        console.log(event);
         event.preventDefault();
-        const todoTitle = document.querySelector('input[name=title]').value;
-        props.onAddTodo(todoTitle);
+       // const todoTitle = event.target.title.value;
+        props.onAddTodo({title: todoTitle});
         document.querySelector('form').reset();
     };
 
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title </label>
-            <input type="text" id="todoTitle" name="title" />
+            <input type="text" id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange} />
             <button type="submit">Add</button>
         </form>
     );
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,29 +2,10 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    {
-      id: 1,
-      title: 'Complete assignment',
-    },
-    {
-      id: 2,
-      title: 'Eat',
-    },
-    {
-      id: 3,
-      title: 'Sleep',
-    },
-    {
-      id: 4,
-      title: 'Repeat',
-    },
-  
-];
-
 
 //Declare a function named TodoList
-function TodoList() {
+function TodoList({todoList}) {
+
   return (
     <div>
       <ul>
