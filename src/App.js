91e88a75fc58aac798aa946e97063101fb773bec
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-11-29 11:17:47 -0500

    Local Storage with List saved as Object

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import AddTodoForm from './AddTodoForm';
 
 
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
+  //const [todoList, setTodoList] = React.useState([]);
 
-  const [setNewTodo] = React.useState('');
+  //const [setNewTodo] = React.useState('');
   //const [newTodo, setNewTodo] = React.useState('');
   //const [addTodo,newTodo ] = React.useState('');
+
+  const [todoList, setTodoList] = React.useState(
+		JSON.parse(localStorage.getItem("savedTodoList"))
+	);
 
   React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
