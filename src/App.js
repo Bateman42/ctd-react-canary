82ee19c2c0557ea540793c55b0e89e54b7d9dc70
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-12-03 09:51:03 -0500

    Create Custom Hook and Fragments completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-
-function App() {
-  //const [todoList, setTodoList] = React.useState([]);
-
-  //const [setNewTodo] = React.useState('');
-  //const [newTodo, setNewTodo] = React.useState('');
-  //const [addTodo,newTodo ] = React.useState('');
+function useSemiPersistentState() {
 
   const [todoList, setTodoList] = React.useState(
 		JSON.parse(localStorage.getItem("savedTodoList"))
@@ -18,18 +12,22 @@ function App() {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
 
-  function addTodo(newTodo){
-    setTodoList([...todoList, newTodo]);
-    return(<div></div>);
-  };
+  return [todoList, setTodoList];
+}
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+  
+  function addTodo(newTodo) {
+		setTodoList([...todoList, newTodo]);
+	}
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
       <TodoList todoList={todoList}/>
-    </div>
-    // <p>{newTodo.title}</p>
+    </>
   );
 }
  
