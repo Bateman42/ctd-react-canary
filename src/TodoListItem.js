b9cde881c10f72d9f1009443431d07eded140840
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2022-01-26 10:14:39 -0500

    1.8 lesson completed and cleened code

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,19 +1,17 @@
 // Import React from "react" npm package
 import React from 'react';
 
-//Declare a function named TodoListItem
-
-let TodoListItem = ({todo, onRemoveTodo}) => {
+const TodoListItem = ({id, title, onRemoveTodo}) => {
+    const headleRemoveTodo = (e) => {
+        onRemoveTodo(id);
+    };
     return (
-    <li> 
-        {todo.title}
-        <button type="button" onClick={ () => {
-            onRemoveTodo(todo.id)
-        }}>Remove</button>
+    <li key="{id}"> 
+        {title}
+        <button onClick={headleRemoveTodo}>Remove</button>
     </li>
     );
 };
 
-/// Export TodoListItem function as default module
 export default TodoListItem; 
 
